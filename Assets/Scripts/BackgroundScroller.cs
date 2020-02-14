bfaf349c6ae93c27ab5832d544294072cdc52916
paladinze <paladinze@hotmail.com>
2020-02-14 17:36:53 +0800

    added particle system

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float scrollSpeed = 0.25f;
+    Material bgMaterial;
+    Vector2 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0f, scrollSpeed);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
